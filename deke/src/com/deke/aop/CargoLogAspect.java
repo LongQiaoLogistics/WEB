0411032ec2dda6dce3940380ec69a2e7845f66dc
叶龙 <270174017@qq.com>
2016-05-15 16:10:02 +0800

    dd

--- deke/src/com/deke/aop/CargoLogAspect.java
+++ deke/src/com/deke/aop/CargoLogAspect.java
@@ -7,8 +7,9 @@ import org.aspectj.lang.JoinPoint;
 
 public class CargoLogAspect {
 	private Logger logger = Logger.getLogger(getClass());
-	
+	//System.out.println("hello world");
 	public void before(JoinPoint jp){
 		logger.debug(jp.getSignature().getName()+"");
+		//ÄãºÃ
 	}
 }
